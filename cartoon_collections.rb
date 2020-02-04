1837b3fa7677581a45966f2f06f26330ef907f1c
Spencer Blum <spencerblum@gmail.com>
2020-02-04 00:50:58 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -17,7 +17,7 @@ def long_planeteer_calls(array)# code an argument here
   # Your code here
   result = array.any?{|words|  words.length > 4 }
 
-  
+
 end
 
 def find_the_cheese(array)# code an argument here
