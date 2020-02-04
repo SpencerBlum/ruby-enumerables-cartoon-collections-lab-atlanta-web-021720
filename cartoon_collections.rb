53f802e728ae251c9ce5c457236e0a9a2bc85595
Spencer Blum <spencerblum@gmail.com>
2020-02-04 00:14:47 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,5 +1,11 @@
-def roll_call_dwarves# code an argument here
+def roll_call_dwarves(array)
+
+  array.each_with_index do |dwarf, index|
+    puts "#{index + 1}. #{dwarf}"
+  
+  # code an argument here
   # Your code here
+
 end
 
 def summon_captain_planet# code an argument here
