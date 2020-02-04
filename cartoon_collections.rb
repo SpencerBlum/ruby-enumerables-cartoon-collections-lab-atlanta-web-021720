746916f89e7f05ced6f8c5496e34dc03b7fc527a
Spencer Blum <spencerblum@gmail.com>
2020-02-04 00:39:18 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -17,7 +17,7 @@ def long_planeteer_calls(array)# code an argument here
   # Your code here
   result = array.map.any?{|words|  words.length > 3 }
 
-  if !result
+  if result
     result = false
 
   end
