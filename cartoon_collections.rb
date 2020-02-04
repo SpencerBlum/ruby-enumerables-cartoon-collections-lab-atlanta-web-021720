82f371ac44dc63159540991d7c3387248accb740
Spencer Blum <spencerblum@gmail.com>
2020-02-04 00:32:46 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -13,8 +13,10 @@ def summon_captain_planet(array)# code an argument here
   array.map{|name| name.capitalize + "!"}
 end
 
-def long_planeteer_calls# code an argument here
+def long_planeteer_calls(array)# code an argument here
   # Your code here
+  array.map.any?{|words|  words.ch > 3 }
+  
 end
 
 def find_the_cheese# code an argument here
